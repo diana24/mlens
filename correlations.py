4fde477c54e531d158917bd279af36fe241d4785
diana24 <dyana_blue_24@yahoo.com>
2016-06-05 14:59:07 +0300

    moved normalize function

--- correlations.py
+++ correlations.py
@@ -1,3 +1,11 @@
+import numpy
+import scipy
+
+def normalize(ratings):
+	meanr = numpy.mean(ratings)
+	sd = numpy.std(ratings,ddof=1)
+	norm_ratings = (ratings - meanr)/sd
+	return norm_ratings
 
 def pearson_similarity(list1, list2):
     out_len = min(len(list1), len(list2))
