aac24b2cb2d162671b17efe2f930e238e16d438f
nicupavel <npavel@mini-box.com>
2016-06-04 21:23:55 -0400

    Job 2: Just output a simple average

--- mlens.py
+++ mlens.py
@@ -4,13 +4,14 @@ from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 from mrjob.step import MRStep
 from operator import itemgetter
+from itertools import combinations
 import sys
-
-def avg_sim(rating_pairs):
-	return 1
 
 class MRMlens(MRJob):
 
+	# JOB 1
+	# ----------------------------------------
+	# Mapper: (user_id) ((movie_id),(rating))
 	def mapper_arrange(self, _, line):
 		try:
 			line=line.strip()
@@ -22,7 +23,7 @@ class MRMlens(MRJob):
 		except:
 			pass
 
-
+	# Reducer: (user_id) ((movie_id, rating), (movie_id, rating)...)
 	def reducer_arrange(self, user_id, movie_rating_pairs):
 		user_ratings = []
 		for movie_id, rating in movie_rating_pairs:
@@ -30,26 +31,32 @@ class MRMlens(MRJob):
 
 		yield user_id, user_ratings
 
+	# JOB 2
+	# ----------------------------------------
+	# Mapper: (movie_id, movie_id) (rating, rating)
+	def mapper_groupmv(self, user_id, movie_rating_pairs):
+		# Generate pairs (movie_id, movie_id) (rating, rating)
+		# from all combinations of ((movie_id, rating), (movie_id, rating)...)
+		for pair1, pair2 in combinations(movie_rating_pairs, 2):
+		    yield (pair1[0], pair2[0]), (pair1[1], pair2[1])
 
-	def mapper_groupmv(self,user_id,line):
-		mvr_pairs = line.split(" ")
-		for i in range(0,len(mvr_pairs)-2):
-			for j in range(i+1,len(mvr_pairs)-1):
-				movie_id1 = mvr_pairs[i].split(",")[0]
-				movie_id2 = mvr_pairs[j].split(",")[0]
-				rating1 = mvr_pairs[i].split(",")[1]
-				rating2 = mvr_pairs[j].split(",")[1]
-				if(movie_id1 < movie_id2):
-					yield movie_id1+','+movie_id2, rating1+','+rating2
-				else:
-					yield movie_id2+','+movie_id1, rating2+','+rating1
-	def reducer_groupmv(self,mvpair,rating_pairs):
-		sim = avg_sim(rating_pairs)
-		yield mvpair, sim
+	# Reducer: (movie_id, movie_id) (similarity)
+	def reducer_groupmv(self, movies_pair, ratings_pair):
+		cnt = 0
+		avg = 0
+		movie1, movie2 = movies_pair # movies_pair is a generator
+		for rating0, rating1 in ratings_pair:
+		    avg = avg + (float(rating0) + float(rating1))/2.0
+		    cnt += 1
+		avg = avg / cnt
+		yield (movie1, movie2), (avg, cnt)
+
+
 	def steps(self):
 		return [
-			MRStep(mapper=self.mapper_arrange ,reducer = self.reducer_arrange),
-			#MRStep(mapper=self.mapper_groupmv,reducer = self.reducer_groupmv)
-		]	
+			MRStep(mapper=self.mapper_arrange, reducer = self.reducer_arrange),
+			MRStep(mapper=self.mapper_groupmv, reducer = self.reducer_groupmv)
+		]
+
 if __name__ == '__main__':
 	MRMlens.run()	
